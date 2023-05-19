dd7ce7c71d67af8c7d8b93a5df14d915e52806ad
Warhair <tegzesmarton@gmail.com>
2023-05-19 10:31:46 +0200

    eventhez_adatok

---
+++ adatok.js
@@ -0,0 +1,6 @@
+export const EVENTLISTA = [
+    {cim: "Minotaur", leiras: "Találkozol egy minotaurral veszítesz egy hp-t", eletero: -1, kaja: 0},
+    {cim: "Kötszer", leiras: "Találtál kötszert a földön gyógyulsz egy hp-t", eletero: 1, kaja: 0},
+    {cim: "Éhinség", leiras: "Megéheztél a hosszú úton veszítesz 5 kaját", eletero: 0, kaja: -5},
+    {cim: "Elátmány", leiras: "Találtál ellátmányt a földön kapsz 5 kaját", eletero: 0, kaja: 5},
+]

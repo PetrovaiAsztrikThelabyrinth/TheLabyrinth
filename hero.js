ba17309479703b91219961683ad431d0f6c7738f
TarGergo <tar.gergo21@gmail.com>
2023-05-25 11:46:27 +0200

    img a html-ben hero added keypessed

--- hero.js
+++ hero.js
@@ -6,7 +6,9 @@ class Hero{
     }
 
 }
-function herolocation(){}
+function herolocation(){
+    
+}
 function stat(palya){
   let elet =  5;
   let stamina = Math.floor(Math.random() * (palya%20)) + 1;

639f7d98bdae6ba05f8959851150177dd33bed69
TarGergo <tar.gergo21@gmail.com>
2023-05-25 11:10:56 +0200

    let / hero

--- hero.js
+++ hero.js
@@ -7,7 +7,21 @@ class Hero{
 
 }
 function stat(palya){
-   elet =  5;
-   stamina = Math.floor(Math.random() * (palya%20)) + 1;
+  let elet =  5;
+  let stamina = Math.floor(Math.random() * (palya%20)) + 1;
+}
+function keyPressed(){
+    if (keyCode === LEFT_ARROW){
+      x-= 10
+    }
+    if (keyCode === UP_ARROW){
+      y-= 10
+    }
+    if (keyCode === RIGHT_ARROW){
+      x-= -10
+    }
+    if (keyCode === DOWN_ARROW){
+      y-= -10
+    }
 }
 export default Hero;

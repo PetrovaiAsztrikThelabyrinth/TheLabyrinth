d14c84a6b75e27511e2ea29626c85aac03f50976
Petrovai Asztrik <asztrikpetrovai@gmail.com>
2023-05-25 11:39:23 +0200

    random hős levéve

--- JatekTer.js
+++ JatekTer.js
@@ -2,10 +2,10 @@ import Tile from "./Tile.js";
 import Hero from "./hero.js";
 import { TILE } from "./adatok.js";
 class JatekTer {
-#meret = 10; 
-#palya = this.#meret*this.#meret;
-constructor(){
-  const hos = new Hero(this.#palya);
+  #meret = 5;
+  #palya = this.#meret * this.#meret;
+  constructor() {
+    const hos = new Hero(this.#palya);
     console.log("Jatekter");
     const ARTICLE = $(".border");
     let hero = this.herolocation();
@@ -14,17 +14,9 @@ constructor(){
       let tiletextures = this.tiletexture();
       const tile = new Tile(ARTICLE, i, TILE[tiletextures]);
     }
+  }
+  tiletexture() {
+    return Math.floor(Math.random() * 3);
+  }
 }
-tiletexture(){
- return Math.floor(Math.random() * 3);
-}
-
-herolocation(){
-  return Math.floor(Math.random() * 5);
- }
-
- veglocation(){
-  return Math.floor(Math.random() * 5)+(this.#palya-5);
- }
-}
-export default JatekTer
+export default JatekTer;

dfc0e14a5af8c09dc1bcbc81e9ecaa6f35ce29cf
Petrovai Asztrik <asztrikpetrovai@gmail.com>
2023-05-25 11:04:33 +0200

    vegpont random

--- JatekTer.js
+++ JatekTer.js
@@ -1,15 +1,19 @@
 import Tile from "./Tile.js";
+import Hero from "./hero.js";
 import { TILE } from "./adatok.js";
 class JatekTer {
 #meret = 10; 
 #palya = this.#meret*this.#meret;
 constructor(){
+    hero
     console.log("Jatekter");
     const ARTICLE = $(".border");
     let hero = this.herolocation();
+    let veg = this.veglocation();
     for (let i = 0; i < this.#palya; i++) {
       let tiletextures = this.tiletexture();
       if(hero == i){const tile = new Tile(ARTICLE, i, "kepek/hero.png");}
+      if else 
       else{const tile = new Tile(ARTICLE, i, TILE[tiletextures]);}
     }
 }
@@ -22,7 +26,7 @@ herolocation(){
  }
 
  veglocation(){
-  return Math.floor(Math.random() * 5)+95;
+  return Math.floor(Math.random() * 5)+(this.#palya-5);
  }
 }
 export default JatekTer

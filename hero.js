8545ed83ab4ef232f9dc45acb94c2547deac96e5
TarGergo <tar.gergo21@gmail.com>
2023-05-25 11:04:44 +0200

    hero stat

--- hero.js
+++ hero.js
@@ -1,8 +1,10 @@
-class hero{
+import JatekTer from "./JatekTer.js";
+
+class Hero{
     stat();
 }
-stat(){
+function stat(palya){
    elet =  5;
-   stamina = 
+   stamina = Math.floor(Math.random() * (palya%20)) + 1;
 }
-export default hero;
+export default Hero;
